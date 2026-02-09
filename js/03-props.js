1ebd17a01674dc8f5a7dd8084e81634829c0eba4
Olexiy-repin <repinlyoha@gmail.com>
2026-02-09 17:18:08 +0200

    preparation for classwork

---
+++ js/03-props.js
@@ -0,0 +1,18 @@
+/*
+ * Об'єкти
+ *
+ * - Короткі властивості
+ * - Обчислювальні властивості
+ */
+
+//* Короткі властивості
+// const username = 'Mango';
+// const userEmail = 'mango@mail.com';
+
+//* Обчислювальні властивості
+/* 
+  <input type="text" name="color">
+*/
+
+// const inputName = 'color';
+// const inputValue = 'rgb(0,0,0)';
---
+++ js/04-iteration.js
@@ -0,0 +1,36 @@
+/*
+ * Перебір об'єкта
+ *
+ * - Цикл for...in
+ * - Метод Object.keys
+ * - Метод Object.values
+ */
+
+// TODO: Порахуйте загальну кількість відгуків.
+
+//~ Через цикл for...in
+// const feedback = {
+//   good: 5,
+//   neutral: 10,
+//   bad: 3,
+// };
+
+// console.log(feedback);
+
+//~ Через метод Object.keys() та цикл for...of
+// const feedback = {
+//   good: 5,
+//   neutral: 10,
+//   bad: 3,
+// };
+
+// console.log('feedback:', feedback);
+
+//~ Через метод Object.values() та цикл for...of
+// const feedback = {
+//   good: 5,
+//   neutral: 10,
+//   bad: 3,
+// };
+
+// console.log('feedback:', feedback);
