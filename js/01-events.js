820945281111f914e64c0836311182e653e0ddbe
Olexiy-repin <repinlyoha@gmail.com>
2026-03-04 21:37:37 +0200

    classwork

--- js/01-events.js
+++ js/01-events.js
@@ -7,7 +7,39 @@
  * - on*: onSubjectEvent
  */
 
+// const handleClickBtnClick = () => {};
+// const clickBtnClickHandler = () => {};
+// const onClickBtnClick = () => {};
+
 /*
 TODO: Натискаючи на кнопку "Click me" змусь
 TODO: червоний квадрат зміщуватись на 50px по діагоналі
 */
+const refs = {
+  clickBtn: document.querySelector('.js-click'),
+  resetBtn: document.querySelector('.js-reset'),
+  box: document.querySelector('.js-box'),
+};
+
+let step = 0;
+
+const onClickBtnClick = () => {
+  step += 50;
+
+  refs.box.style.transform = `translate(${step}px, ${step}px)`;
+
+  if (step === 200) {
+    refs.clickBtn.removeEventListener('click', onClickBtnClick);
+  }
+};
+
+const onResetBtnClick = () => {
+  step = 0;
+
+  refs.box.style.transform = `translate(${step}px, ${step}px)`;
+
+  refs.clickBtn.addEventListener('click', onClickBtnClick);
+};
+
+refs.clickBtn.addEventListener('click', onClickBtnClick);
+refs.resetBtn.addEventListener('click', onResetBtnClick);
--- js/03-forms.js
+++ js/03-forms.js
@@ -11,4 +11,24 @@ const refs = {
   feedbackForm: document.querySelector('.js-feedback-form'),
 };
 
-console.log(refs);
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+
+  const formData = {
+    [refs.feedbackForm.elements.username.name]: refs.feedbackForm.elements.username.value.trim(),
+    [refs.feedbackForm.elements.email.name]: refs.feedbackForm.elements.email.value.trim(),
+    [refs.feedbackForm.elements.comment.name]: refs.feedbackForm.elements.comment.value.trim(),
+  };
+
+  const formDataValues = Object.values(formData);
+
+  if (formDataValues.includes('')) {
+    alert('Всі поля мають бути заповненими!');
+
+    return;
+  }
+
+  console.log(formData);
+};
+
+refs.feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
